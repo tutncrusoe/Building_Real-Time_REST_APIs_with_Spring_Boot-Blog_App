f44be3174fda4e629f5d9470cc4089744cb4560b
tutncrusoe <huydinhanhle@gmail.com>
2023-02-20 22:54:15 +0700

    CRUD for Post resources

--- src/main/java/com/example/springbootblogrestapi/controller/PostController.java
+++ src/main/java/com/example/springbootblogrestapi/controller/PostController.java
@@ -4,16 +4,15 @@ import com.example.springbootblogrestapi.payload.PostDto;
 import com.example.springbootblogrestapi.service.impl.PostService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/posts")
 public class PostController {
 
-    private PostService postService;
+    private final PostService postService;
 
     public PostController(PostService postService) {
         this.postService = postService;
@@ -23,4 +22,28 @@ public class PostController {
     public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto) {
         return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
     }
+
+    @GetMapping
+    public List<PostDto> getAllPosts() {
+        return postService.getAllPosts();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PostDto> getPostById(@PathVariable(name = "id") long id) {
+        return ResponseEntity.ok(postService.getPostById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PostDto> updatePostById(@RequestBody PostDto postDto,
+                                              @PathVariable(name = "id") long id) {
+        return new ResponseEntity<>(postService.updatePost(postDto, id), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deletePostById(@PathVariable(name = "id") long id) {
+        postService.deletePostById(id);
+        return new ResponseEntity<>("Post entity deleted successfully", HttpStatus.OK);
+    }
+
+
 }
--- src/main/java/com/example/springbootblogrestapi/service/impl/PostService.java
+++ src/main/java/com/example/springbootblogrestapi/service/impl/PostService.java
@@ -1,12 +1,25 @@
 package com.example.springbootblogrestapi.service.impl;
 
 import com.example.springbootblogrestapi.entity.Post;
+import com.example.springbootblogrestapi.exception.ResourceNotFoundException;
 import com.example.springbootblogrestapi.payload.PostDto;
 import com.example.springbootblogrestapi.repository.PostRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 public interface PostService {
     PostDto createPost(PostDto postDto);
+
+    List<PostDto> getAllPosts();
+
+    PostDto getPostById(long id);
+
+    PostDto updatePost(PostDto postDto, long id);
+
+    void deletePostById(long id);
 }
 
 @Service
@@ -18,22 +31,64 @@ class PostServiceImpl implements PostService {
         this.postRepository = postRepository;
     }
 
-    @Override
-    public PostDto createPost(PostDto postDto) {
 
-        // convert DTO to Entity
+    // convert Entity into DTO
+    private PostDto mapToDto(Post post) {
+        PostDto postDto = new PostDto();
+        postDto.setId(post.getId());
+        postDto.setTitle(post.getTitle());
+        postDto.setDescription(post.getDescription());
+        postDto.setContent(post.getContent());
+        return postDto;
+    }
+
+    private Post mapToEntity(PostDto postDto) {
         Post post = new Post();
         post.setTitle(postDto.getTitle());
         post.setDescription(postDto.getDescription());
         post.setContent(postDto.getContent());
+        return post;
+    }
+
+    @Override
+    public PostDto createPost(PostDto postDto) {
+
+        // convert DTO to Entity
+        Post post = mapToEntity(postDto);
         Post newPost = postRepository.save(post);
 
         // convert Entity to DTO
-        PostDto postResponse = new PostDto();
-        postResponse.setTitle(newPost.getTitle());
-        postResponse.setDescription(newPost.getDescription());
-        postResponse.setContent(newPost.getContent());
-
-        return postResponse;
+        return mapToDto(newPost);
     }
+
+    @Override
+    public List<PostDto> getAllPosts() {
+        List<Post> posts = postRepository.findAll();
+        // return posts.stream().map(post -> mapToDto(post)).collect(Collectors.toList());
+        return posts.stream().map(this::mapToDto).collect(Collectors.toList());
+    }
+
+    @Override
+    public PostDto getPostById(long id) {
+        Post existingPost = postRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Post", "id", id));
+        return mapToDto(existingPost);
+    }
+
+    @Override
+    public PostDto updatePost(PostDto postDto, long id) {
+        Post existingPost = postRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Post", "id", id));
+
+        existingPost.setTitle(postDto.getTitle());
+        existingPost.setDescription(postDto.getDescription());
+        existingPost.setContent(postDto.getContent());
+        Post savedPost = postRepository.save(existingPost);
+        return mapToDto(savedPost);
+    }
+
+    @Override
+    public void deletePostById(long id) {
+        Post existingPost = postRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Post", "id", id));
+        postRepository.deleteById(id);
+    }
+
 }
