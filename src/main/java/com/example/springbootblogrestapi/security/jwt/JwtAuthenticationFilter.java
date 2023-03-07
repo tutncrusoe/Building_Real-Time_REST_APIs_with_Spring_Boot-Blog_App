dce886ac865e3f5596ed42943a1a7a6912108a61
tutncrusoe <huydinhanhle@gmail.com>
2023-03-07 10:52:24 +0700

    Creating JwtAuthenticationFilter

---
+++ src/main/java/com/example/springbootblogrestapi/security/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,67 @@
+package com.example.springbootblogrestapi.security.jwt;
+
+
+import com.example.springbootblogrestapi.security.impl.UserDetailsServiceImpl;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    private final UserDetailsServiceImpl userDetailsService;
+
+    public JwtAuthenticationFilter(JwtTokenProvider jwtTokenProvider, UserDetailsServiceImpl userDetailsService) {
+        this.jwtTokenProvider = jwtTokenProvider;
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+
+        // get JWT (tokenĐ from http request
+        String token = getJwtTokenFromRequest(request);
+        // validate token
+        if (StringUtils.hasText(token) && jwtTokenProvider.validateToken(token)) {
+            // get username from token
+            String username = jwtTokenProvider.getUsernameFromJwt(token);
+            // load user associated with token
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+            // create authenticationToken
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                    userDetails,
+                    null,
+                    userDetails.getAuthorities()
+            );
+            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            // set spring security
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    // Bearer <accessToken>
+    private String getJwtTokenFromRequest(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7, bearerToken.length());
+        }
+        return null;
+    }
+
+
+}
